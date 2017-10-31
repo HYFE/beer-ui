abb8c778c61c165bd9246b6cebc8d89dffd42d3f
yscoder <634206017@qq.com>
2017-10-31 21:06:15 +0800

    rebuild docs

--- config/index.js
+++ config/index.js
@@ -8,11 +8,11 @@ const path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../docs/index.html'),
+    index: path.resolve(__dirname, '../index.html'),
     template: path.resolve(__dirname, '../examples/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: '',
-    assetsPublicPath: '../dist/',
+    assetsPublicPath: '/dist/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
